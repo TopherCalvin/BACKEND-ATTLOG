a240d58013271f4635ac3876ea6efd196abfcdc2
TopherCalvin <126538023+TopherCalvin@users.noreply.github.com>
2023-05-19 11:00:26 +0700

    up

--- Absensi APP copy/src/routes/user.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const userController = require("../controllers").userController;
-
-router.post("/v2", userController.insertUser);
-router.get("/v1", userController.getUser);
-
-module.exports = router;
